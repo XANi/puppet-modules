82379d12cb5507f71e12a9f0af131f2eb2a30dae
Mariusz Gronczewski <xani666@gmail.com>
2013-03-19 23:35:07 +0100

    wifi config

--- modules/wireless/manifests/master.pp
+++ modules/wireless/manifests/master.pp
@@ -2,9 +2,21 @@
 #
 # stuff needed for linux to work as AP
 #
-class wireless::master {
+class wireless::master (
+    $interface    = 'wlan0',
+    $ssid         = 'setupme',
+    $country_code = 'US',
+    $hw_mode      = 'g',
+    $channel      = fqdn_rand(8,1),
+    $password,
+) {
     include wireless::util
     package {'hostapd':
         ensure => 'installed',
+    }
+    file {'/etc/hostapd/hostapd.conf':
+        content => template('wireless/hostapd.conf'),
+        mode    => 600,
+        owner   => root,
     }
 }
