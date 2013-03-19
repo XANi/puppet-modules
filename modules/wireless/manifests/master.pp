7715b125617a4f6c2c98fcad3a59178650bbd2d3
Mariusz Gronczewski <xani666@gmail.com>
2013-03-19 23:53:34 +0100

    manage service

--- modules/wireless/manifests/master.pp
+++ modules/wireless/manifests/master.pp
@@ -3,11 +3,11 @@
 # stuff needed for linux to work as AP
 #
 class wireless::master (
-    $interface    = 'wlan0',
-    $ssid         = 'setupme',
-    $country_code = 'US',
-    $hw_mode      = 'g',
-    $channel      = fqdn_rand(8,1),
+    $interface = 'wlan0',
+    $ssid      = 'setupme',
+    $country   = 'US',
+    $hw_mode   = 'g',
+    $channel   = fqdn_rand(8,1),
     $password,
 ) {
     include wireless::util
@@ -18,5 +18,20 @@ class wireless::master (
         content => template('wireless/hostapd.conf'),
         mode    => 600,
         owner   => root,
+        notify  => Service['hostapd'],
+    }
+    file {'/etc/default/hostapd':
+        content => template('wireless/hostapd.default'),
+        mode    => 644,
+        owner   => root,
+        notify  => Service['hostapd'],
+    }
+    service {'hostapd':
+        ensure => running,
+        enable => true,
+        require => [
+                    File['/etc/hostapd/hostapd.conf'],
+                    File['/etc/default/hostapd'],
+                    ]
     }
 }
