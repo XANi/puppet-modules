31d9b6ea6ab6bce70a78a01f47322d98313dfc7f
Mariusz Gronczewski <xani666@gmail.com>
2013-05-30 17:29:27 +0200

    nullmailer skeleton class

--- modules/mpd/manifests/init.pp
+++ modules/mpd/manifests/init.pp
@@ -1,4 +1,10 @@
-class mpd::server {
+class mpd::server (
+    music_dir = '/var/lib/mpd/music',
+    playlist_dir = '/var/lib/mpd/playlists',
+    bind_to = '0.0.0.0',
+    zeroconf = false,
+)
+{
     package { 'mpd':
         ensure => installed,
     }
---
+++ modules/nullmailer/manifests/init.pp
@@ -0,0 +1,31 @@
+
+class nullmailer (
+    $remote,
+    $mailname = $fqdn,
+    $adminaddr = false,
+    $maildomain = $domain,
+) {
+    file {'/etc/mailname':
+        content => $mailname,
+        mode    => 644,
+        owner   => root,
+    }
+    if $adminaddr {
+        file {'/etc/nullmailer/adminaddr':
+            content => $adminaddr,
+            mode    => 644,
+            owner   => root,
+        }
+    }
+    file {'/etc/nullmailer/remotes':
+        content => $remote,
+        mode    => 600,
+        owner   => root,
+    }
+    file {'/etc/nullmailer/defaultdomain':
+        content => $maildomain,
+        mode    => 644,
+        owner   => root,
+    }
+}
+
