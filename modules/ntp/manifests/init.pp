5c5df7d23ac84a8f48513eb6e90a8945e6cbb4f4
Mariusz Gronczewski <xani666@gmail.com>
2013-06-02 22:21:02 +0200

    update time on server change/module creation

--- modules/ntp/manifests/init.pp
+++ modules/ntp/manifests/init.pp
@@ -3,9 +3,17 @@ class ntp::client ($server = hiera('ntp_server','pl.pool.ntp.org')) {
     package { 'ntpdate':
         ensure => installed;
     }
+    $command = "/usr/sbin/ntpdate -t 60 ${server}"
     file { '/etc/cron.hourly/ntpdate':
-        mode  => 755,
-        owner => root,
-        content => "#!/bin/sh\nntpdate -t 60 $server >/dev/null 2>&1\n",
+        mode    => 755,
+        owner   => root,
+        content => "#!/bin/sh\n${command} >/dev/null 2>&1\n",
+        notify  => Exec['update-time'],
     }
+    exec {'update-time':
+        logoutput   => true,
+        command     => $command,
+        refreshonly => true,
+    }
+
 }
