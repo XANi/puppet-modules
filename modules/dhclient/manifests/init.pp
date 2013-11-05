57c67e7684f04883e88eca38bb9fa27da7e142bf
Mariusz Gronczewski <xani666@gmail.com>
2013-11-05 23:16:35 +0100

    bare dhclient

---
+++ modules/dhclient/manifests/init.pp
@@ -0,0 +1,16 @@
+# Class: dhclient
+#
+# hiera:
+#   dhcp:
+#       domain-name: sth
+#       host-name: sth-else
+
+class dhclient {
+    $dhcp = hiera('dhcp', false)
+    file { '/etc/dhcp/dhclient.conf':
+        content => template('dhclient/dhclient.conf.erb'),
+        owner   => root,
+        group   => root,
+        mode    => 644,
+    }
+}
