89fc14145f464238bef0666ce6bdf136ac25a6ca
Mariusz Gronczewski <xani666@gmail.com>
2014-01-06 22:13:12 +0100

    fix for rpi wifi

--- modules/hw/manifests/init.pp
+++ modules/hw/manifests/init.pp
@@ -10,3 +10,11 @@ class hw::iwlwifi {
         ensure => installed,
     }
 }
+
+# disable power maangement, makes wifi on rpi awfully slow
+class hw::8192cu::power {
+    file {'/etc/modprobe.d/8192cu.conf':
+        content => template('hw/8192cu.conf'),
+        mode    => 644,
+    }
+}
