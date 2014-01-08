72174ced2ce5620fb0599c76b0c4886ff5bee09b
Mariusz Gronczewski <xani666@gmail.com>
2014-01-08 23:46:33 +0100

    basic starbound class

---
+++ modules/starbound/manifests/init.pp
@@ -0,0 +1,30 @@
+# Class: starbound::server
+#
+# configure starbound server from steam
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+#
+# Sample Usage:
+#
+#
+#
+
+class starbound::server (
+    $user = 'steam',
+    $path = '/home/steam/steam_apps/starbound',
+    $steam_script_dir = '/home/steam/scripts',
+    $steam_user       = false
+    ) {
+    if $steam_user {
+        file {"${steam_script_dir}/update_starbound.steam":
+            content => template('starbound/update_starbound.steam'),
+            owner   => $user,
+            mode    => 644,
+        }
+    }
+}
