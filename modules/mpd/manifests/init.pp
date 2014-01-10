ad8a18112cdc0da057d51c6bf776e9f2a67c6c69
Mariusz Gronczewski <xani666@gmail.com>
2014-01-10 02:16:45 +0100

    erp

--- modules/mpd/manifests/init.pp
+++ modules/mpd/manifests/init.pp
@@ -1,10 +1,9 @@
 class mpd::server (
-    music_dir = '/var/lib/mpd/music',
-    playlist_dir = '/var/lib/mpd/playlists',
-    bind_to = '0.0.0.0',
-    zeroconf = false,
-)
-{
+    $music_dir = '/var/lib/mpd/music',
+    $playlist_dir = '/var/lib/mpd/playlists',
+    $bind_to = '0.0.0.0',
+    $zeroconf = false,
+) {
     package { 'mpd':
         ensure => installed,
     }
