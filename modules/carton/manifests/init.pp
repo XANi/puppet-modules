1c0afea206c2940a6d89fee7a683aef0de8a7dea
Mariusz Gronczewski <xani666@gmail.com>
2014-02-09 17:23:08 +0100

    carton module

---
+++ modules/carton/manifests/init.pp
@@ -0,0 +1,39 @@
+
+class carton::common {
+    package {'carton':
+        ensure => installed,
+    }
+}
+
+
+# Define: carton::app
+#
+# Deploy perl carton app
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+#
+# Sample Usage:
+#
+#
+#
+define carton::app (
+    $dir,
+    $user = 'root',
+    )  {
+    include perl::carton::common
+    Exec {
+        path      => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+        user      => $user,
+        cwd       => $dir,
+        logoutput => true,
+    }
+    exec {"carton-install-${title}":
+        command => 'carton install >.installed',
+        onlyif  => 'test -e .installed && test cpanfile -ot .installed',
+    }
+}
