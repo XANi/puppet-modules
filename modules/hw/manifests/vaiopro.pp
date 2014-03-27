1725a9e21f7979899da01d8f648b5b254801cf31
Mariusz Gronczewski <xani666@gmail.com>
2014-03-27 21:52:17 +0100

    vaio touchpad

---
+++ modules/hw/manifests/vaiopro.pp
@@ -0,0 +1,15 @@
+# Class: hw::vaiopro
+class hw::vaiopro {
+    if !defined(File['/etc/X11/xorg.conf.d']) {
+        file { '/etc/X11/xorg.conf.d':
+            ensure => directory,
+            mode   => 644,
+            owner  => root,
+        }
+    }
+    file{'/etc/X11/xorg.conf.d/99-vaio-synaptics.conf':
+        content => template('hw/vaio-synaptics.conf'),
+        owner   => root,
+        mode    => 644,
+    }
+}
