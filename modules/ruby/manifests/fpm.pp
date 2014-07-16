d711e7d8d1c9a5b26ad7ef55d83edd1cb7576322
Mariusz Gronczewski <xani666@gmail.com>
2014-07-16 14:28:55 +0200

    add dep dir

--- modules/ruby/manifests/fpm.pp
+++ modules/ruby/manifests/fpm.pp
@@ -5,10 +5,15 @@ class ruby::fpm (
     $install_path = '/var/fpm',
     ) {
     include ruby
+    file { $install_path:
+        ensure => directory,
+        owner  => $user,
+        mode   => 750,
+    }
     vcsrepo { $install_path:
         source   => "https://github.com/jordansissel/fpm",
         ensure   => present,
-        provider => git,
+        provider => 'git',
         user     => $user,
         revision => $version,
     }
