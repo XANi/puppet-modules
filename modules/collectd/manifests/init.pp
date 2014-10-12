673e31375ee837bbe84daedbb881ca4640fa0e46
Mariusz Gronczewski <xani666@gmail.com>
2014-10-13 01:47:09 +0200

    change perms

--- modules/collectd/manifests/init.pp
+++ modules/collectd/manifests/init.pp
@@ -21,7 +21,7 @@ class collectd::common {
     }
     file {'/etc/collectd/conf.d':
         ensure  => directory,
-        mode    => 600,
+        mode    => 644,
         owner   => root,
         recurse => true,
         purge   => true,
