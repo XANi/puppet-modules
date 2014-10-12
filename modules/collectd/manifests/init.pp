31564dd785d9d4e489491129e439c768fc75bf38
Mariusz Gronczewski <xani666@gmail.com>
2014-10-12 23:00:32 +0200

    simple collectd mod

---
+++ modules/collectd/manifests/init.pp
@@ -0,0 +1,28 @@
+class collectd::client ($server) {
+    include collectd::common
+    package {'collectd':
+        ensure => installed
+    }
+    file {'/etc/collectd/collectd.conf':
+        content => template('collectd/collectd.conf'),
+        owner   => root,
+    }
+}
+
+class collectd::common {
+    package {'collectd':
+        ensure => installed
+    }
+    file {'/etc/collectd/conf.d':
+        ensure  => directory,
+        mode    => 600,
+        owner   => root,
+        recurse => true,
+        purge   => true,
+        force   => true,
+    }
+    file {'/etc/default/collectd':
+        content => template('collectd/default'),
+    }
+
+}
