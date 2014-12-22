5da19ee61bbf1f5a96366b057e86884198da41da
Mariusz Gronczewski <xani666@gmail.com>
2014-12-22 16:49:14 +0100

    some pkg tools

---
+++ modules/util/manifests/deb.pp
@@ -0,0 +1,10 @@
+# set of tools needed to make deb packages
+
+class util::deb::pkgmaker {
+    package { [
+               'dh-make',
+               'debhelper',
+               'fakeroot',
+               'linitian',]:
+    }
+}
