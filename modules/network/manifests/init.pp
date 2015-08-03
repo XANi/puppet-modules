9154ff4b405614b3408ad897386a4b49680bc62e
Mariusz Gronczewski <xani666@gmail.com>
2015-08-03 22:26:37 +0200

    manage global file

--- modules/network/manifests/init.pp
+++ modules/network/manifests/init.pp
@@ -1,4 +1,6 @@
-class network {
+class network (
+    $manage_global = false # also manage global file
+)   {
     if $::osfamily =~ /(?i:debian)/ {
         include network::common::debian
     }
@@ -8,7 +10,9 @@ class network {
 }
 
 
+
 class network::common::debian {
+    require network
     file {'/etc/network/interfaces.d/':
         ensure  => directory,
         recurse => true,
@@ -16,6 +20,17 @@ class network::common::debian {
         owner   => root,
         mode    => 644,
     }
+    if $network::manage_global {
+        concat { '/etc/network/interfaces':
+            ensure => present,
+        }
+        concat::fragment {'network interface header':
+            target  => '/etc/network/interfaces',
+            content => template('network/if-debian-main'),
+            order   => '00',
+        }
+    }
+
 }
 
 class network::common::redhat {
