20d714de23d08b781772e064e57ebab936f9dbe2
Mariusz Gronczewski <xani666@gmail.com>
2017-01-18 05:57:27 +0100

    apt stuff

--- modules/core/manifests/init.pp
+++ modules/core/manifests/init.pp
@@ -4,4 +4,10 @@ class core {
 
 class core::apt::base {
     create_resources("@apt::source",hiera('repos'))
+    file {'/etc/apt/apt.conf.d/99-zpuppet.conf':
+        ensure => absent,
+    }
+    apt::conf {"no-suggested":
+        content => 'APT::Install-Suggests "0";'
+    }
 }
