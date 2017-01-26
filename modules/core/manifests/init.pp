0f30a03a6a7cec02bf8e8fb847887cbed636146d
Mariusz Gronczewski <xani666@gmail.com>
2017-01-26 12:49:28 +0100

    fix

--- modules/core/manifests/init.pp
+++ modules/core/manifests/init.pp
@@ -1,5 +1,17 @@
+class core::desktop {
+    include core::apt::base
+}
+
+class core::server {
+    include core::apt::base
+    apt::conf {"no-suggested":
+        content => 'APT::Install-Suggests "0";'
+    }
+}
+
 class core {
     include core::apt::base
+    notify {"Please use core::server or core::desktop":;}
 }
 
 class core::apt::base {
@@ -7,7 +19,5 @@ class core::apt::base {
     file {'/etc/apt/apt.conf.d/99-zpuppet.conf':
         ensure => absent,
     }
-    apt::conf {"no-suggested":
-        content => 'APT::Install-Suggests "0";'
-    }
+
 }
