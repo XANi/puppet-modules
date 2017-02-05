fac1c557feace331f9e7b4724ca6093efd486be1
Mariusz Gronczewski <xani666@gmail.com>
2017-02-05 20:18:07 +0100

    add core class

--- modules/core/manifests/init.pp
+++ modules/core/manifests/init.pp
@@ -21,3 +21,38 @@ class core::apt::base {
     }
 
 }
+
+class core::server (
+    $ntp_client = true,
+    $collectd_client = true,
+) {
+    if $collectd_client {
+        include collectd::client
+    }
+    if $ntp_client {
+        include ntp::client
+    }
+    $interval = 600 + fqdn_rand(20)
+    class { dpp:
+        manifest_from => 'private',
+        poll_interval => $hostname ? {
+            default => $interval,
+        },
+        minimum_interval => $hostname ? {
+           hastur => 120,
+           default => fqdn_rand(120,240),
+        },
+    }
+    include common::utils
+    include common::cleanup
+    include util::shell
+    include user::common
+    include motd
+    include puppet
+    rsyslog::log {'puppet':;}
+    rsyslog::log {'dpp':;}
+        file {'/usr/local/bin/e':
+        target => '/usr/bin/zile',
+    }
+
+}
