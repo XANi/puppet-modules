4027f1d96e2b14919bc766847644fe6a6fc34fb4
Mariusz Gronczewski <xani666@gmail.com>
2017-02-21 00:05:15 +0100

    fix

--- modules/dpp/manifests/init.pp
+++ modules/dpp/manifests/init.pp
@@ -80,7 +80,7 @@ class dpp (
     }
 
     exec {"get-dpp-archive":
-        command => "wget -O /opt/dpp/dpp.${architecture}.tmp https://github.com/XANi/go-dpp/releases/download/v0.0.2/",
+        command => "wget -O /opt/dpp/dpp.${architecture}.tmp https://github.com/XANi/go-dpp/releases/download/v0.0.2/dpp.${architecture}",
         creates => "/opt/dpp/dpp.${architecture}.tmp",
         notify => Exec['verify-dpp-archive'],
     }
