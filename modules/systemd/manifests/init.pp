ea9ef0a399abb3641485934d91aa6bc55c213e80
Mariusz Gronczewski <xani666@gmail.com>
2017-04-04 08:22:29 +0200

    svc handler

--- modules/systemd/manifests/init.pp
+++ modules/systemd/manifests/init.pp
@@ -8,3 +8,57 @@ class systemd::poweroff_on_halt {
         owner  => root,
     }
 }
+class systemd::common {
+    exec {"systemd-reload":
+        command => "systemctl daemon-reload",
+        refreshonly => true,
+    }
+}
+
+
+define systemd::service::override (
+    $service_name = $title,
+    $service = false,
+    $unit = false,
+    $install = false,
+    $prio = 1000,
+    ) {
+    $padded_prio = sprintf('%04d',$prio)# 4 -> 0004
+    include systemd::common
+    if !defined (File["/etc/systemd/system/${service_name}.service.d/"]) {
+        file {"/etc/systemd/system/${service_name}.service.d/":
+            ensure  => directory,
+            recurse => true,
+            purge   => true,
+            mode    => 644,
+        }
+    }
+    file {"/etc/systemd/system/${service_name}.service.d/${padded_prio}-${title}.conf":
+        content => template('systemd/service'),
+        mode => 644,
+        owner => root,
+        group => root,
+        notify => Exec["systemd-reload"],
+    }
+
+}
+
+define systemd::service (
+    $content = undef,
+    $source  = undef,
+    ) {
+    include systemd::common
+    if !$content and !$source {
+        $content_c = template("systemd/service/${title}.conf")
+    }
+    else {
+        $content_c = $content
+    }
+
+    file {"/etc/systemd/system/${title}.service":
+        content => $content_c,
+        source  => $source,
+        mode    => 644,
+        notify  => Exec["systemd-reload"]
+    }
+}
