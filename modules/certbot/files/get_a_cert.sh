df57fecb709d17f141028b5038fce5a50588c653
Mariusz Gronczewski <xani666@gmail.com>
2018-04-10 22:36:57 +0200

    certbot support

---
+++ modules/certbot/files/get_a_cert.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# puppet
+exit 1
+certs=$(perl -e 'print map {"-d $_ "} @ARGV' $@)
+/usr/bin/certbot certonly \
+    --keep-until-expiring  \
+    --expand \
+    --agree-tos  \
+    --non-interactive \
+    --no-self-upgrade \
+    --post-hook "/bin/systemctl reload haproxy" \
+    --webroot -w /var/www/certbot  \
+    $certs
+
+for dir in $(ls -d /etc/letsencrypt/live/*); do
+    cat $dir/privkey.pem $dir/fullchain.pem > $dir/certandkey.pem
+done
---
+++ modules/certbot/manifests/init.pp
@@ -0,0 +1,22 @@
+class certbot::common {
+    file { '/usr/local/bin/get_a_cert.sh':
+        source => "puppet:///modules/certbot/get_a_cert.sh",
+        mode   => "755",
+    }
+    package {'certbot': ensure => latest}
+}
+
+define certbot::cert (
+    $main_domain,
+    $extra_domain,
+)   {
+    include certbot::common
+    $domains = inline_template('<%= Array([@main_domain, @extra_domain]).flatten.join(" ")')
+    cron{"certbot_${title}":
+        command => "/usr/local/bin/get_a_cert.sh ${domains}",
+        weekday => fqdn_rand(6),
+        hour    => fqdn_rand(6),
+        minute  => fqdn_rand(59),
+    }
+
+}
