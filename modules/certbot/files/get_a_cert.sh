9e19120df96a360fc1b183b72af8af2394ca9780
Mariusz Gronczewski <xani666@gmail.com>
2018-10-27 10:44:06 +0200

    delay haproxy reload

--- modules/certbot/files/get_a_cert.sh
+++ modules/certbot/files/get_a_cert.sh
@@ -5,7 +5,7 @@ if [ "z$1" = "zreload" ] ; then
     for dir in $(ls -d /etc/letsencrypt/live/*); do
         cat $dir/privkey.pem $dir/fullchain.pem > $dir/certandkey.pem
     done
-    systemctl reload haproxy
+    /bin/bash -c 'sleep 1m ;systemctl reload haproxy' &
 else
     certs=$(perl -e 'print map {"-d $_ "} @ARGV' $@)
     /usr/bin/certbot certonly \
