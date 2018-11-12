a493e3f8d8d2ba99805ad306069969437a4cb5fa
Mariusz Gronczewski <xani666@gmail.com>
2018-11-12 18:50:41 +0100

    common class

---
+++ modules/common/manifests/server.pp
@@ -0,0 +1,47 @@
+class common::server (
+    # serve ntp to others
+    $ntp_server = false,
+)  {
+    class { 'apt':
+        purge => {
+            'sources.list'   => true,
+            'sources.list.d' => true,
+            'preferences'    => false,
+            'preferences.d'  => false,
+        }
+    }
+    include core
+    include core::monitoring
+    realize Apt::Source['main-stretch']
+    include puppet
+    include common::utils
+    include common::cleanup
+    include util::shell
+    include user::common
+    include motd
+    if ($hostname != 'cthulhu') {
+        include collectd::client
+    }
+    if $ntp_server {
+        include ntp::server
+    } else {
+        include ntp::client
+    }
+    # cleanup that shit, some packages depend on it for no good reason
+    package { ['apache2', 'apache2-bin']:
+        ensure => absent,
+    }
+    file {'/usr/local/bin/e':
+        target => '/usr/bin/zile',
+    }
+    $interval = 3600 + fqdn_rand(20)
+    if !defined(Class['dpp']) {
+        class { dpp:
+            manifest_from => 'private',
+            poll_interval => $interval,
+            minimum_interval => 300,
+        }
+    }
+    rsyslog::log {'puppet':;}
+    rsyslog::log {'dpp':;}
+}
