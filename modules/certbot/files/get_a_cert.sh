271d011f5cdcb96bfcb6008578cec421e417bfc6
Mariusz Gronczewski <xani666@gmail.com>
2019-03-03 19:21:20 +0100

    fix

--- modules/certbot/files/get_a_cert.sh
+++ modules/certbot/files/get_a_cert.sh
@@ -3,7 +3,7 @@
 SELF=$(readlink -f $0)
 
 if [ "z$1" = "zreload" ] ; then
-    for dir in $(ls -d /etc/letsencrypt/live/*); do
+    for dir in $(find  /etc/letsencrypt/live/ -maxdepth 1 -mindepth 1 -type d); do
         cat $dir/privkey.pem $dir/fullchain.pem > $dir/certandkey.pem
     done
     /bin/bash -c 'sleep 1m ;systemctl reload haproxy' &
