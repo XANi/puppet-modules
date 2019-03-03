bee27984935af26cc1c204119047040e82ba240b
Mariusz Gronczewski <xani666@gmail.com>
2019-03-03 19:16:40 +0100

    fix

--- modules/certbot/manifests/init.pp
+++ modules/certbot/manifests/init.pp
@@ -4,12 +4,27 @@ class certbot::common {
         source => "puppet:///modules/certbot/get_a_cert.sh",
         mode   => "755",
     }
+    if !defined(File['/etc/pki']) {
+        file { '/etc/pki':
+            ensure => directory,
+            owner  => root,
+            mode   => '711',
+        }
+    }
+    file { '/etc/pki/certbot':
+        ensure => directory,
+        owner  => root,
+        mode   => '711',
+    }
     package {'certbot': ensure => latest}
 }
 
 define certbot::cert (
     $main_domain,
     $extra_domain,
+    $owner = 'root',
+    $group = 'root',
+    $mode  = '600',
 )   {
     include certbot::common
     $domains = inline_template('<%= Array([@main_domain, @extra_domain]).flatten.join(" ") %>')
@@ -19,5 +34,10 @@ define certbot::cert (
         hour    => fqdn_rand(6),
         minute  => fqdn_rand(59),
     }
+    file { "/etc/pki/certbot/${title}.pem":
+        source => "/etc/letsencrypt/live/${main_domain}/certandkey.pem",
+        notify => $notify,
+    }
+
 
 }
