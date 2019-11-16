a39859fe25600bb45f4b62903d6527d646a6e966
Mariusz Gronczewski <xani666@gmail.com>
2019-11-16 09:43:42 +0100

    fix

--- modules/common/manifests/cleanup.pp
+++ modules/common/manifests/cleanup.pp
@@ -1,1 +1,11 @@
 
+# remove unwanted crap
+class common::cleanup {
+    # these are outdated crappy and unwanted
+    package {[
+        'manpages-pl',
+        'manpages-pl-dev',
+    ]:
+        ensure => absent,
+    }
+}
