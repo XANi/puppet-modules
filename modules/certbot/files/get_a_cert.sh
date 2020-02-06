26c687f59e1bca592ae0ae1c9c16174f03905e62
Mariusz Gronczewski <xani666@gmail.com>
2020-02-06 18:02:28 +0100

    fix cert bundle update

--- modules/certbot/files/get_a_cert.sh
+++ modules/certbot/files/get_a_cert.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 # puppet
-SELF=$(readlink -f $0)
+certs=$(perl -e 'print map {"-d $_ "} @ARGV' $@)
+/usr/bin/certbot certonly \
+                 --cert-name $1 \
+                 --keep-until-expiring  \
+                 --expand \
+                 --agree-tos  \
+                 --non-interactive \
+                 --no-self-upgrade \
+                 --webroot -w /var/www/certbot  \
+                 $certs
+# if any of the certs got updated, update the certandkey bundle
+RELOAD=0
+for dir in $(find  /etc/letsencrypt/live/ -maxdepth 1 -mindepth 1 -type d); do
+   if [ "$dir/fullchain.pem" -nt "$dir/certandkey.pem" ] ; then
+       echo "updating cert in $dir"
+       RELOAD=1
+       cat $dir/privkey.pem $dir/fullchain.pem > $dir/certandkey.pem
+    fi
+done
 
-if [ "z$1" = "zreload" ] ; then
-    for dir in $(find  /etc/letsencrypt/live/ -maxdepth 1 -mindepth 1 -type d); do
-        cat $dir/privkey.pem $dir/fullchain.pem > $dir/certandkey.pem
-    done
-    /bin/bash -c 'sleep 1m ;systemctl reload haproxy' &
-else
-    certs=$(perl -e 'print map {"-d $_ "} @ARGV' $@)
-    /usr/bin/certbot certonly \
-                     --cert-name $1 \
-                     --keep-until-expiring  \
-                     --expand \
-                     --agree-tos  \
-                     --non-interactive \
-                     --no-self-upgrade \
-                     --post-hook "$SELF reload" \
-                     --webroot -w /var/www/certbot  \
-                     $certs
+# and reload haproxy
+
+if [ "$RELOAD" = "1" ]; then
+    echo "certs changed, reloading haproxy"
+    systemctl reload haproxy
 fi
