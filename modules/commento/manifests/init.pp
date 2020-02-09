dfc7a891b1fec7f2a081596fb0558f53fb9e425c
Mariusz Gronczewski <xani666@gmail.com>
2020-02-09 11:48:06 +0100

    commento

--- modules/commento/manifests/init.pp
+++ modules/commento/manifests/init.pp
@@ -1,10 +1,25 @@
 class commento::server (
-
+    $origin_domain,
+    $port = 3010,
+    $postgres_url = "postgres://localhost:5432",
 ) {
     realize Group["commento"]
     realize User["commento"]
     systemd::service { 'commento':
         content => template('commento/commento.service'),
+    }
+    # squeaky secrets there so put it away from /etc
+    file { '/opt/commento/cfg':
+        ensure => directory,
+        owner  => commento,
+        mode   => "700",
+    }
+    file { '/opt/commento/cfg/env.cfg':
+        content => "# puppet managed file
+COMMENTO_POSTGRES=${postgres_url}
+",
+        owner => "commento",
+        mode => "0600",
     }
     service { 'commento':
         ensure => running,
