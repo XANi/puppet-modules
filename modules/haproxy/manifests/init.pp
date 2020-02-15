1b7410144ade95fc979a732dc1e9b2de4abc6976
Mariusz Gronczewski <xani666@gmail.com>
2020-02-15 17:02:09 +0100

    haproxy validation

--- modules/haproxy/manifests/init.pp
+++ modules/haproxy/manifests/init.pp
@@ -8,21 +8,29 @@ class haproxy::server (
         ensure => running,
         enable => true,
     }
-    concat { '/etc/haproxy/haproxy.cfg':
-        mode => "644",
+    concat { '/etc/haproxy/.haproxy.cfg.v':
+        mode => "600",
         notify => Service['haproxy'],
-
+        owner => root,
+    }
+    file { '/etc/haproxy/haproxy.cfg':
+        subscribe => Concat['/etc/haproxy/.haproxy.cfg'],
+        source    => '/etc/haproxy/.haproxy.cfg',
+        mode      => "640",
+        owner     => "haproxy",
+        group     => "haproxy",
+        validate_cmd => '/usr/sbin/haproxy -c -f %',
     }
     if $global_content {
         concat::fragment { 'haproxy_global':
-            target  => '/etc/haproxy/haproxy.cfg',
+            target  => '/etc/haproxy/.haproxy.cfg.v',
             content => "${global_content}\n",
             order   => 0,
         }
     }
     if $global_content {
         concat::fragment { 'haproxy_defaults':
-            target  => '/etc/haproxy/haproxy.cfg',
+            target  => '/etc/haproxy/.haproxy.cfg.v',
             content => "${defaults_content}\n",
             order   => 1,
         }
@@ -35,7 +43,7 @@ define haproxy::config (
     $order = 1000,
 )   {
     concat::fragment{ "haproxy_${title}":
-        target  => '/etc/haproxy/haproxy.cfg',
+        target  => '/etc/haproxy/.haproxy.cfg.v',
         content =>  $content,
         order   => $order,
     }
