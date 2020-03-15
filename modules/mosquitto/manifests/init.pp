64e47e4367c604b865640cd9bc69bd8a426e1214
Mariusz Gronczewski <xani666@gmail.com>
2020-03-15 08:28:40 +0100

    mosquitto

--- modules/mosquitto/manifests/init.pp
+++ modules/mosquitto/manifests/init.pp
@@ -7,7 +7,7 @@ class mosquitto::server (
     Boolean $allow_anonymous = false,
     Variant[Boolean[false],String] $persistent_client_expiration = "90d",
     Variant[Boolean[false],String] $password_file = "/etc/mosquitto/passwd",
-    Variant[Boolean[false],String] $server_cert,
+    Variant[Boolean[false],String] $server_cert = false,
 ){
     ensure_packages(['mosquitto'])
     include mosquitto::client
