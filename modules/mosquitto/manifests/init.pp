ef7bbee617f84e6f8d0fdd954639225d6acbcb35
Mariusz Gronczewski <xani666@gmail.com>
2020-03-15 08:45:15 +0100

    mq cert

--- modules/mosquitto/manifests/init.pp
+++ modules/mosquitto/manifests/init.pp
@@ -7,6 +7,7 @@ class mosquitto::server (
     Boolean $allow_anonymous = false,
     Variant[Boolean[false],String] $persistent_client_expiration = "90d",
     Variant[Boolean[false],String] $password_file = "/etc/mosquitto/passwd",
+    Boolean $manage_password_file = true,
     Variant[Boolean[false],String] $server_cert = false,
 ){
     ensure_packages(['mosquitto'])
@@ -17,6 +18,15 @@ class mosquitto::server (
         mode => "640",
         content => template('mosquitto/general.conf')
     }
+    if $password_file and $manage_password_file {
+        concat { $password_file:
+            owner  => mosquitto,
+            group  => mosquitto,
+            mode   => "0600",
+            notify => Class['mosquitto'],
+        }
+    }
+
     service { 'mosquitto':
         ensure => running,
         enable => true,
