83e2ad093c1614e34808cf630cf93e45b0ec082c
Mariusz Gronczewski <xani666@gmail.com>
2022-05-26 19:22:59 +0200

    rpi

--- modules/common/manifests/utils.pp
+++ modules/common/manifests/utils.pp
@@ -4,32 +4,30 @@
 #
 class common::utils {
     include util::etckeeper
-    package {
-        [
-            'aptitude',
-            'bc',
-            'bridge-utils',
-            'bwm-ng',
-            'bzip2',
-            'curl',
-            'ethtool',
-            'locales-all', # so various tools can use it and non-english letters in console work
-            'lsof',
-            'lvm2',
-            'mtr-tiny',
-            'ncdu',
-            'nmap',
-            'psmisc',
-            'sudo',
-            'tcpdump',
-            'tmux',
-            'tree',
-            'xfsprogs',
-            'debsecan',
-            'apt-transport-https',
-            'zile',
-         ]:
-             ensure => installed,
-    }
+    ensure_packages([
+        'aptitude',
+        'bc',
+        'bridge-utils',
+        'bwm-ng',
+        'bzip2',
+        'curl',
+        'ethtool',
+        'locales-all', # so various tools can use it and non-english letters in console work
+        'lsof',
+        'lvm2',
+        'mtr-tiny',
+        'ncdu',
+        'nmap',
+        'psmisc',
+        'sudo',
+        'tcpdump',
+        'tmux',
+        'tree',
+        'xfsprogs',
+        'debsecan',
+        'apt-transport-https',
+        'zile',
+    ])
+
 }
 
