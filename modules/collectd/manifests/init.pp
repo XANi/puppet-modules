4fdf3676952a88b4b160641fe79610e720980ee8
Mariusz Gronczewski <xani666@gmail.com>
2023-03-20 21:14:57 +0100

    collectd things

--- modules/collectd/manifests/init.pp
+++ modules/collectd/manifests/init.pp
@@ -1,231 +1,90 @@
-class collectd::client ($server, $config = false) {
-    include collectd::common
-    # some plugins get wonky sadly (mqtt in 5.7 have problems reconnecting)
-    cron { 'restart-collectd':
-        command => "/bin/systemctl restart collectd",
-        hour    => fqdn_rand(23),
-        minute  => fqdn_rand(59),
+class collectd::common (
+    $version = "installed",
+    $interval = 10,
+) {
+    package {"collectd":
+        ensure => $version,
     }
-    if $config {
-        file { '/etc/collectd/collectd.conf':
-            content => $config,
-            owner   => root,
-            notify  => Service['collectd'],
-        }
-    } else {
-        file { '/etc/collectd/collectd.conf':
-            content => template('collectd/config/default.conf'),
-            owner   => root,
-            notify  => Service['collectd'],
-        }
+#    # some plugins get wonky sadly (mqtt in 5.7 have problems reconnecting)
+#    cron { 'restart-collectd':
+#        command => "/bin/systemctl restart collectd",
+#        hour    => fqdn_rand(23),
+#        minute  => fqdn_rand(59),
+#    }
+    file {[
+        '/etc/collectd',
+        '/etc/collectd/collectd.conf.d',
+    ]:
+        ensure => directory,
+        mode   => "711", # +x so we can use this dir for non-root-running plugin config
     }
-}
-
-class collectd::server($config) {
-    include collectd::common
-    # some plugins get wonky sadly (mqtt in 5.7 have problems reconnecting)
-    cron { 'restart-collectd':
-        command => "/bin/systemctl restart collectd",
-        hour    => fqdn_rand(23),
-        minute  => fqdn_rand(59),
-    }
-    file { '/etc/collectd/collectd.conf':
-        content => $config,
-        owner   => root,
-        notify  => Service['collectd'],
-    }
-}
-
-class collectd::common {
-    service {'collectd':
+    service { 'collectd':
         ensure => running,
         enable => true,
     }
-    package {'collectd':
-        ensure => latest,
+    file { '/usr/share/collectd/types.db':
+        source   => "puppet://puppet/modules/collectd/types.db",
+        mode     => "644",
+        owner    => root,
+        group    => root,
+        require  => Package['collectd'],
+        notify => Service['collectd'],
     }
-    package {'liboping0':
-        ensure => installed,
+    file { '/var/lib/collectd':
+        ensure => directory,
+        mode   => "771",
     }
-    file {'/etc/collectd/conf.d':
-        ensure  => directory,
-        mode    => "644",
-        owner   => root,
-        recurse => true,
-        purge   => true,
-        force   => true,
+    file { '/etc/collectd/custom_types.db':
+        source   => "puppet://puppet/modules/collectd/custom_types.db",
+        mode     => "644",
+        notify => Service['collectd'],
     }
-    file {'/etc/collectd/conf.d/local.conf':
-        replace => false,
-        content => template('collectd/empty.conf');
+    file { '/etc/collectd/collectd.conf':
+        content => template('collectd/collectd.conf'),
+        mode    => "600",
+        notify => Service['collectd'],
     }
-    file {'/etc/default/collectd':
-        content => template('collectd/default'),
-    }
-    @collectd::plugin::load {
-        'logfile':;
-        'syslog':;
-        'log_logstash':;
-        'section                                                         #':;
-        'aggregation':;
-        'amqp':;
-        'apache':;
-        'apcups':;
-        'ascent':;
-        'barometer':;
-        'battery':;
-        'bind':;
-        'ceph':;
-        'cgroups':;
-        'chrony':;
-        'conntrack':;
-        'contextswitch':;
-        'cpu':;
-        'cpufreq':;
-        'cpusleep':;
-        'csv':;
-        'curl':;
-        'curl_json':;
-        'curl_xml':;
-        'dbi':;
-        'df':;
-        'disk':;
-        'dns':;
-        'dpdkevents':;
-        'dpdkstat':;
-        'drbd':;
-        'email':;
-        'entropy':;
-        'ethstat':;
-        'exec':;
-        'fhcount':;
-        'filecount':;
-        'fscache':;
-        'gmond':;
-        'gps':;
-        'hugepages':;
-        'grpc':;
-        'hddtemp':;
-        'intel_rdt':;
-        'interface':;
-        'ipc':;
-        'ipmi':;
-        'iptables':;
-        'ipvs':;
-        'irq':;
-        'java':;
-        'load':;
-        'lua':;
-        'lvm':;
-        'madwifi':;
-        'mbmon':;
-        'mcelog':;
-        'md':;
-        'memcachec':;
-        'memcached':;
-        'memory':;
-        'modbus':;
-        'mqtt':;
-        'multimeter':;
-        'mysql':;
-        'netlink':;
-        'network':;
-        'nfs':;
-        'nginx':;
-        'notify_desktop':;
-        'notify_email':;
-        'notify_nagios':;
-        'ntpd':;
-        'numa':;
-        'nut':;
-        'olsrd':;
-        'onewire':;
-        'openldap':;
-        'openvpn':;
-        'ovs_events':;
-        'ovs_stats':;
-        'perl':;
-        'pinba':;
-        'ping':;
-        'postgresql':;
-        'powerdns':;
-        'processes':;
-        'protocols':;
-        'python':;
-        'redis':;
-        'rrdcached':;
-        'rrdtool':;
-        'sensors':;
-        'serial':;
-        'sigrok':;
-        'smart':;
-        'snmp':;
-        'snmp_agent':;
-        'statsd':;
-        'swap':;
-        'table':;
-        'tail':;
-        'tail_csv':;
-        'tcpconns':;
-        'teamspeak2':;
-        'ted':;
-        'thermal':;
-        'tokyotyrant':;
-        'turbostat':;
-        'unixsock':;
-        'uptime':;
-        'users':;
-        'uuid':;
-        'varnish':;
-        'virt':;
-        'vmem':;
-        'vserver':;
-        'wireless':;
-        'write_graphite':;
-        'write_http':;
-        'write_kafka':;
-        'write_log':;
-        'write_mongodb':;
-        'write_prometheus':;
-        'write_redis':;
-        'write_riemann':;
-        'write_sensu':;
-        'write_tsdb':;
-        'xencpu':;
-        'zfs_arc':;
-        'zookeeper':;
-    }
-    user { 'collectd':
-        system => true,
-        ensure => present
+    file { '/etc/collectd/collectd.conf/0000-filters.conf':
+        content => template('collectd/filters.conf'),
+        mode    => "600",
+        notify => Service['collectd'],
     }
 }
-define collectd::plugin (
+
+define collectd::conf(
+    $content = undef,
+    $source = undef,
     $prio = 1000,
-    $params = {},
-    $template = $title,
-)  {
-    $padded_prio = sprintf('%04d',$prio)
-    $file_content = template("collectd/plugins/${template}.conf")
-    file { "/etc/collectd/conf.d/${padded_prio}-${title}.conf":
-        content => "${file_content}\n", # always add trailing newline
+) {
+    $padded_prio = sprintf('%04d', $prio)
+
+    if !$content or !$source {
+        fail('need $content or $source')
+    }
+    if $content {
+        $content_c = "# ${title}\n${content}\n"
+    }
+    file { "/etc/collectd/collectd.conf/${padded_prio}-${title}.conf":
+        source  => $source,
+        content => $content_c,
         mode    => "600",
-        owner   => root,
-        notify  => Service['collectd'],
+        notify => Service['collectd'],
     }
 }
-define collectd::plugin::load (
-    $interval = false,
-)   {
-    $padded_prio ="0000"
-    $plugin = $title
-    $file_content = template("collectd/load_plugin.conf")
-    file { "/etc/collectd/conf.d/${padded_prio}-load_${title}.conf":
-        content => "${file_content}\n", # always add trailing newline
-        mode    => "600",
-        owner   => root,
-        notify  => Service['collectd'],
+
+define collectd::network::send(
+    $address,
+    $port=25826,
+) {
+    collectd::conf { "network-${title}":
+        content => "<Plugin network>\nServer \"<%= @address %>\" \"<%= @port %>\"\n</Plugin>\n"
     }
+}
+
+
+
+class collectd::client ($server) {
+    include collectd::common
 }
 
 define collectd::plugin::exec (
@@ -235,15 +94,9 @@ define collectd::plugin::exec (
 )  {
     include collectd::common
     realize Collectd::Plugin::Load[exec]
-    collectd::plugin {
-        "exec_${title}":
-            template => 'exec',
-            params => {
-                command => $command,
-                user    => $user,
-                args    => $args,
-            },
-            notify => Service['collectd'],
+    collectd::conf { "exec_${title}":
+        content => template('collectd/p/exec.conf')
     }
+
 }
 
