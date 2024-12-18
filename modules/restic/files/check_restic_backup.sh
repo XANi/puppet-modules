70eddf3f6865a3d914952bc2e2e30ba7774b0b6c
Mariusz Gronczewski <xani666@gmail.com>
2024-12-18 21:00:48 +0100

    automatically count backups in check

--- modules/restic/files/check_restic_backup.sh
+++ modules/restic/files/check_restic_backup.sh
@@ -7,16 +7,24 @@ CURRENT_DATE=$(date "+%F")
 YESTERDAY=$(date "+%F" -d yesterday)
 EXIT=0
 expected=$(find /etc/restic/jobinfo -type f |wc -l)
-backups_in_date=$(restic snapshots --latest 1|grep -P "($CURRENT_DATE|$YESTERDAY)" |wc -l)
+tmpf=$(mktemp)
+restic snapshots --latest 1|grep -P "($CURRENT_DATE|$YESTERDAY)" >$tmpf
+backups_in_date=$(cat $tmpf |wc -l)
+
 if [ "$backups_in_date" -eq 0 ] ; then
     echo "no backup found!";
+    cat $tmpf
+    rm -f $tmpf
     exit 2
 fi
 if [ "$backups_in_date" -lt $expected ] ; then
     echo "expected $expected backups, got $backups_in_date"
-    restic snapshots --latest 1|grep -P "($CURRENT_DATE|$YESTERDAY)"
+    cat $tmpf
+    rm -f $tmpf
     exit 1
 else
     echo "all OK: $backups_in_date"
-exit 0
+    cat $tmpf
+    rm -f $tmpf
+    exit 0
 fi
