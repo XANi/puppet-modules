dcc3c01c3f1e741fd50be7625c51c76c712af2f3
Mariusz Gronczewski <xani666@gmail.com>
2024-12-19 17:10:18 +0100

    fix

--- modules/restic/files/check_restic_backup.sh
+++ modules/restic/files/check_restic_backup.sh
@@ -5,7 +5,7 @@ source /etc/restic/env
 LC_ALL=C
 CURRENT_DATE=$(date "+%F")
 YESTERDAY=$(date "+%F" -d yesterday)
-EXIT=0
+EXIT=3
 expected=$(find /etc/restic/jobinfo -type f |wc -l)
 tmpf=$(mktemp)
 restic snapshots --latest 1|grep -P "($CURRENT_DATE|$YESTERDAY)" >$tmpf
@@ -13,18 +13,19 @@ backups_in_date=$(cat $tmpf |wc -l)
 
 if [ "$backups_in_date" -eq 0 ] ; then
     echo "no backup found!";
-    cat $tmpf
-    rm -f $tmpf
-    exit 2
-fi
-if [ "$backups_in_date" -lt $expected ] ; then
+    EXIT=2
+elif [ "$backups_in_date" -eq $expected ] ; then
+    echo "$backups_in_date backups OK"
+elif [ "$backups_in_date" -lt $expected ] ; then
     echo "expected $expected backups, got $backups_in_date"
-    cat $tmpf
-    rm -f $tmpf
-    exit 1
+    EXIT=1
+elif [ "$backups_in_date" -gt $expected ] ; then
+    echo "more than expected [$expected] backup count: $backups_in_date"
+    EXIT=1
 else
-    echo "all OK: $backups_in_date"
-    cat $tmpf
-    rm -f $tmpf
-    exit 0
+    echo "should not be here"
+    EXIT=2
 fi
+cat $tmpf
+rm -f $tmpf
+exit $EXIT
