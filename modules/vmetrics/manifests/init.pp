3ad8ac753bf8183ac471028bbc2dc0707650ca27
Mariusz Gronczewski <xani666@gmail.com>
2025-01-19 23:38:29 +0100

    backup

--- modules/vmetrics/manifests/init.pp
+++ modules/vmetrics/manifests/init.pp
@@ -131,7 +131,7 @@ define vmetrics::snapshot (
 ) {
     include vmetrics::snapshot::common
     cron { "vmetrics-snapshot-${title}":
-        command => "/opt/vmetrics/bin/vmsnapshot.sh ${port} ${title} >>/tmp/vmsnapshot.log 2>&1",
+        command => "/opt/vmetrics/bin/vmsnapshot.sh ${port} ${title} 2>&1 | ts >>/tmp/vmsnapshot.log",
         hour    => 1,
         minute  => fqdn_rand(59, $title),
         user => 'vmetrics',
