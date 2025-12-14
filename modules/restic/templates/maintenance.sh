39c8720c1d06d2f571ee9eefc993817d1eefaeee
Mariusz Gronczewski <xani666@gmail.com>
2025-12-14 05:23:32 +0100

    garage backup

--- modules/restic/templates/maintenance.sh
+++ modules/restic/templates/maintenance.sh
@@ -7,7 +7,7 @@ source /etc/restic/env
 # just in case/for first run
 restic unlock || restic init
 restic forget --tag "" --keep-daily 7 --keep-weekly <%= @weekly %>  --keep-monthly <%= @monthly %>
-restic forget --tag hourly --keep-hourly 48 --keep-daily 14 --keep-weekly <%= @weekly %>  --keep-monthly <%= @monthly %>
-restic forget --tag daily --keep-daily 7 --keep-weekly <%= @weekly %>  --keep-monthly <%= @monthly %>
-restic forget --tag weekly --keep-daily 1 --keep-weekly <%= @weekly %>  --keep-monthly <%= @monthly %>
+restic forget --tag hourly --keep-hourly 48 --keep-daily 14 --keep-weekly <%= @weekly %>  --keep-monthly <%= @monthly %> --keep-within <%= @monthly %>m
+restic forget --tag daily --keep-daily 7 --keep-weekly <%= @weekly %>  --keep-monthly <%= @monthly %> --keep-within <%= @monthly%>m
+restic forget --tag weekly --keep-daily 1 --keep-weekly <%= @weekly %>  --keep-monthly <%= @monthly %> --keep-within <%= @monthly%>m
 restic prune
