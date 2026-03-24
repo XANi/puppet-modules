b9ceef33689b2711f20b70f25e46dd64f3626e81
Mariusz Gronczewski <xani666@gmail.com>
2026-03-24 16:37:25 +0100

    oltp exporter

--- modules/rustfs/manifests/init.pp
+++ modules/rustfs/manifests/init.pp
@@ -8,6 +8,7 @@ class rustfs::server(
     $console_address='0.0.0.0:9001',
     $manage_user = true,
     $manage_datadir = true,
+    # without /v1/metrics part
     $oltp_exporter_url = false,
 ) {
     if $manage_user {
